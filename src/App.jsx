f30e1c892bf62aad52563d7939b5584b0a324f89
Lux VEGBA <vegbahermerite@gmail.com>
2025-04-24 12:42:59 +0200

    perf : code structure perf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
-import Register from './components/Register';
-import Home from './components/Home';
+import Register from './pages/Register.jsx';
+import Home from './pages/Home.jsx';
 import './App.css'
 
 function App() {
